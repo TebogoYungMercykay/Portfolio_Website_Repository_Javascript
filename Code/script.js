574a0deb86b53dd03e062665be0288a915d3ae75
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-07-16 21:04:41 +0200

    Javascript: Scrolling Funtionality

--- Code/script.js
+++ Code/script.js
@@ -1,1 +1,48 @@
 // Javascript Code for my Portfolio Website
+
+// toddle icon navbar
+let menuIcon = document.querySelector ('#menu-icon');
+let navbar = document . querySelector ('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// Smooth scrolling
+let sections = document.querySelectorAll ('section');
+let navLinks = document.querySelectorAll ('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute ('id');
+        if((top >= offset) && (top < (offset + height))){
+            // Active navbar Links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // Active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // For an animation that repeats on scroll:
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    // Sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Removing navbar and toddle icon onScroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // Footer animation on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+};
