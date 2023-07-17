fbbe452e07650fe28bff1bbc2cba64b2bb844ffc
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-07-17 18:13:17 +0200

    Contact form Email and SheetDB API Implementation.

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,3 @@
-// Javascript Code for my Portfolio Website
-
 // toddle icon navbar
 let menuIcon = document.querySelector ('#menu-icon');
 let navbar = document . querySelector ('.navbar');
@@ -46,3 +44,42 @@ window.onscroll = () => {
     let footer = document.querySelector('footer');
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 };
+
+// SmtpJS.com:
+// Website: https://smtpjs.com/
+// Script: https://smtpjs.com/v3/smtp.js
+function sendEmail(name, subject, email, mobile, message) {
+    Email.send({
+        SecureToken : "e55a2ab4-902c-4210-9a0a-d3684e57227f",
+        To : 'sbkskhalo.kq@gmail.com',
+        From : email,
+        Subject : "PORTFOLIO: New Contact Form Enquiry",
+        Body : "Name: " + name + "<br> Subject: " + subject + "<br> Email: " + email + "<br> Mobile: " + mobile + "<br> Message: " + message
+    }).then( console.log("Thank You for Contacting Me, You shall have a Great Day ahead."));
+}
+// InputValidation
+function confirmDetails() {
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var email = document.getElementById("email").value;
+    var mobile = document.getElementById("mobile_number").value;
+    var message = document.getElementById("message").value;
+    var check = false;
+    if(check) {
+        sendEmail(name, subject, email, mobile, message);
+    }
+    return check;
+}
+
+var form = document.getElementById("contact-form");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("contact-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+        console.log("Thank You for Contacting Me, You shall have a Great Day ahead.");
+    });
+});
