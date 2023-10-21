b7793f08c3642c86b96b1e4dbae5d4257c2e58f5
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-22 00:04:31 +0200

    Using JSON File to Populate the Social Media Links

--- src/js/social-media.js
+++ src/js/social-media.js
@@ -1,6 +1,7 @@
 fetch('./JSON/social-media.json')
     .then(response => response.json())
     .then(data => {
+        // Contact Page
     const contactSci = document.querySelector('.contact-sci');
     data.forEach(socialMedia => {
         const link = document.createElement('a');
@@ -9,6 +10,16 @@ fetch('./JSON/social-media.json')
         link.rel = 'noopener';
         link.innerHTML = `<i class="bx bxl-${socialMedia.name.toLowerCase()}"></i>`;
         contactSci.appendChild(link);
+    });
+        // Home Page
+    const homeSci = document.querySelector('.home-sci');
+    data.forEach(socialMedia => {
+        const link = document.createElement('a');
+        link.href = socialMedia.url;
+        link.target = '_blank';
+        link.rel = 'noopener';
+        link.innerHTML = `<i class="bx bxl-${socialMedia.name.toLowerCase()}"></i>`;
+        homeSci.appendChild(link);
     });
 })
 .catch(error => console.error('Error fetching social media data:', error));
