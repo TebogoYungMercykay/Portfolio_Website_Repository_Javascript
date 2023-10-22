311daaba4bd6d781362e93863669860d1a983459
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-22 04:14:37 +0200

    Fetching & populating Languages from a JSON file

--- src/js/most-used-languages.js
+++ src/js/most-used-languages.js
@@ -1,18 +1,19 @@
 // Fetching and populating images from the JSON file
 
 fetch('./json/most-used-languages.json')
-  .then(response => response.json())
-  .then(data => {
-    const portfolioContent = document.getElementById('portfolio-content');
+	.then(response => response.json())
+	.then(data => {
+		const portfolioContent = document.getElementById('portfolio-content');
 
-    data.forEach(languageData => {
-        portfolioContent.innerHTML += `<div class="progress">
+		data.forEach(languageData => {
+			portfolioContent.innerHTML += `<div class="progress">
             <h3> ${languageData.name} <span>${languageData.percentage}%</span>
             </h3>
             <div class="bar">
                 <span></span>
             </div>
         </div>`;
-    });
-})
-.catch(error => console.error('Error fetching data:', error));
+		});
+	})
+	.catch(error => console.error('Error fetching data:', error));
+
