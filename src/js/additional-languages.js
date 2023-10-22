5ad337479723bea100fa01590d40c4127fd84220
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-22 03:59:27 +0200

    Dynamically Generating the Languages Data

---
+++ src/js/additional-languages.js
@@ -0,0 +1,18 @@
+// Fetching and populating images from the JSON file
+
+fetch('./json/additional-languages-data.json')
+  .then(response => response.json())
+  .then(data => {
+    const portfolioContent = document.getElementById('portfolio-content-2');
+
+    data.forEach(languageData => {
+        portfolioContent.innerHTML += `<div class="progress">
+            <h3> ${languageData.name} <span>${languageData.percentage}%</span>
+            </h3>
+            <div class="bar">
+                <span></span>
+            </div>
+        </div>`;
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
