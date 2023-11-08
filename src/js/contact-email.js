bf374bc7cdde6cb2f3e6dbd6f0eafde98848c1d9
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-11-08 11:46:26 +0200

    Fixed the API For Sending Form Data

--- src/js/contact-email.js
+++ src/js/contact-email.js
@@ -9,36 +9,56 @@ function sendEmail(name, subject, email, mobile, message) {
 		To: 'sbkskhalo.kq@gmail.com',
 		From: email,
 		Subject: "PORTFOLIO: New Contact Form Enquiry",
-		Body: "Name: " + name + "<br> Subject: " + subject + "<br> Email: " + email + "<br> Mobile: " + mobile + "<br> Message: " + message
+		Body: `Name: ${name}<br> Subject: ${subject}<br> Email: ${email}<br> Mobile: ${mobile}<br> Message: ${message}`
 	}).then(alert("Thank You for Contacting Me, You shall have a Great Day ahead."));
 }
 
 // * InputValidation
 
 function confirmDetails() {
-	// Storing User Inputs
-	var name = document.getElementById("name").value;
-	var subject = document.getElementById("subject").value;
-	var email = document.getElementById("email").value;
-	var mobile = document.getElementById("mobile_number").value;
-	var message = document.getElementById("message").value;
-	var check = false;
-	if (!check) {
-		// Sending an Enquiry Email
-		sendEmail(name, subject, email, mobile, message);
-	}
-	// Clearing Inputs
-	document.getElementById("name").value = "";
-	document.getElementById("subject").value = "";
-	document.getElementById("email").value = "";
-	document.getElementById("mobile_number").value = "";
-	document.getElementById("message").value = "";
-	return check;
+    // Storing User Inputs
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var email = document.getElementById("email").value;
+    var mobile = document.getElementById("mobile_number").value;
+    var message = document.getElementById("message").value;
+    var check = true;
+
+    // Checking if all inputs are not empty
+    if (name == "" || subject == "" || email == "" || mobile == "" || message == "") {
+        alert("All fields must be filled out");
+        check = false;
+    }
+
+    // Checking if name contains only letters
+    if (!/^[a-zA-Z]+$/.test(name)) {
+        alert("Name must contain only letters");
+        check = false;
+    }
+
+    // Checking if email is valid
+    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+        alert("Email must contain a '@' and at least one character on the left and right of '@'");
+        check = false;
+    }
+
+	// Checking if mobile number contains only digits
+    if (!/^\d+$/.test(mobile)) {
+        alert("Mobile number must contain only digits");
+        check = false;
+    }
+
+    if (check == true) {
+        // Sending an Enquiry Email
+        sendEmail(name, subject, email, mobile, message);
+    }
+
+    return check;
 }
 
 var form = document.getElementById("contact-form");
-form.addEventListener("submit", e => {
-	e.preventDefault();
+form.addEventListener("submit", event => {
+	event.preventDefault();
 	fetch(form.action, {
 		method: "POST",
 		body: new FormData(document.getElementById("contact-form")),
@@ -46,5 +66,15 @@ form.addEventListener("submit", e => {
 		response => response.json()
 	).then((html) => {
 		console.log("Thank You for Contacting Me, You shall have a Great Day ahead.");
+        reset();
 	});
 });
+
+function reset() {
+    // Clearing Inputs
+    document.getElementById("name").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("mobile_number").value = "";
+    document.getElementById("message").value = "";
+}
