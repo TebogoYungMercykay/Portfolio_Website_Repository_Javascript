bcd5d8acb44bf5873c93569618748f3864d05d8e
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-03-02 01:46:21 +0200

    Media: Executing All Scripts When DOMContentLoaded

--- src/js/social-media.js
+++ src/js/social-media.js
@@ -1,27 +1,29 @@
 // Fetching and populating images from the JSON file
 
-fetch('./json/social-media.json')
-	.then(response => response.json())
-	.then(data => {
-		// Contact Page
-		const contactSci = document.querySelector('.contact-sci');
-		data.forEach(socialMedia => {
-			const link = document.createElement('a');
-			link.href = socialMedia.url;
-			link.target = '_blank';
-			link.rel = 'noopener';
-			link.innerHTML = `<i class="bx bxl-${socialMedia.name.toLowerCase()}"></i>`;
-			contactSci.appendChild(link);
-		});
-		// Home Page
-		const homeSci = document.querySelector('.home-sci');
-		data.forEach(socialMedia => {
-			const link = document.createElement('a');
-			link.href = socialMedia.url;
-			link.target = '_blank';
-			link.rel = 'noopener';
-			link.innerHTML = `<i class="bx bxl-${socialMedia.name.toLowerCase()}"></i>`;
-			homeSci.appendChild(link);
-		});
-	})
-	.catch(error => console.error('Error fetching social media data:', error));
+document.addEventListener('DOMContentLoaded', function() {
+	fetch('./json/social-media.json')
+		.then(response => response.json())
+		.then(data => {
+			// Contact Page
+			const contactSci = document.querySelector('.contact-sci');
+			data.forEach(socialMedia => {
+				const link = document.createElement('a');
+				link.href = socialMedia.url;
+				link.target = '_blank';
+				link.rel = 'noopener';
+				link.innerHTML = `<i class="bx bxl-${socialMedia.name.toLowerCase()}"></i>`;
+				contactSci.appendChild(link);
+			});
+			// Home Page
+			const homeSci = document.querySelector('.home-sci');
+			data.forEach(socialMedia => {
+				const link = document.createElement('a');
+				link.href = socialMedia.url;
+				link.target = '_blank';
+				link.rel = 'noopener';
+				link.innerHTML = `<i class="bx bxl-${socialMedia.name.toLowerCase()}"></i>`;
+				homeSci.appendChild(link);
+			});
+		}
+	).catch(error => console.error('Error fetching social media data:', error));
+});
